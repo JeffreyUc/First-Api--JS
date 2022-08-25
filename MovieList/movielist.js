23e28ee37a30064481d85525c51b0f2c36e8b852
Uche Jeffrey <uchennaekwomadu@gmail.com>
2022-08-25 13:49:47 +0100

    Update

--- MovieList/movielist.js
+++ MovieList/movielist.js
@@ -1,4 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+//get
+router.get('/', (req, res) => {
+    res.send(console.log('check'))
+})
+//get with id
+router.get('/id', (req, res) => {
+
+})
+//post
+router.post('/', (req, res) => {
+
+})
+//put
+router.patch('/id', (req, res) => {
+
+})
+//delete
+router.delete('/id', (req, res) => {
+
+})
+
 module.exports = router
